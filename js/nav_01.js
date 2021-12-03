b5fd965f29fa0a63bb4d85d34f8b60571dd943f6
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-02 21:55:43 -0300

    1.3

---
+++ js/nav_01.js
@@ -0,0 +1,14 @@
+//active code
+let list = document.querySelectorAll('.list');
+
+for (let i = 0; i < list.length; i++) {
+    list[i].onclick = function(){
+        let j = 0;
+        while(j < list.length){
+            list[j++].className = '.list';
+
+        }
+        list[i].className = 'list active';
+    }
+    
+}
