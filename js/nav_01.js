2b2a4a6b1464c45ea73433f2e00a0def57c54c23
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-02 23:06:18 -0300

    2.0

--- js/nav_01.js
+++ js/nav_01.js
@@ -1,6 +1,8 @@
 let menuToggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
 menuToggle.onclick = function() {
     menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
 }
 
 
