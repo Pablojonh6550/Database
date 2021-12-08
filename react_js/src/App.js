5600a8e9b600fa4591566a4a2d21f6cf7189ded4
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-08 20:41:39 -0300

    1.4

--- react_js/src/App.js
+++ react_js/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  // let message = 'Hello World';
+  const [tasks, setTasks] = useState([
+    {
+      id:'1',
+      title:'Estudar programação',
+      completed: false,
+    },
+    {
+      id:'2',
+      title:'Ler Livros',
+      completed: true,
+    }
+  ]);
+
+  return (
+    <>
+      <div className='container'></div>
+      
+    </>
+  );
   
-  return <div className='container'>
-
-  </div>;
-
 }
 
 export default App;
---
+++ react_js/src/components/tasks.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Tasks = () => {
+    return <h1>hello world</h1>;
+}
