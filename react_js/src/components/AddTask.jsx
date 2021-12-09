1faa4e02a428a38e94a4d8f1dadeb839c6e42b91
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-08 22:33:41 -0300

    1.9

--- react_js/src/components/AddTask.jsx
+++ react_js/src/components/AddTask.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './AddTask.css';
 
 const AddTask = () => {
     return (
-        <input type="text" />
+        <div className='add-task-container'>
+            <input className="add-task-input" type="text" />
+        </div>
         
     );
 }
