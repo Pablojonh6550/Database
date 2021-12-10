f0f9114f5bef541b496e6b07a3abe59952a8c39e
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-10 15:31:17 -0300

    3.1

---
+++ react_js/src/components/TaskDetails.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Button from './Button';
+
+const TaskDetails = () => {
+    return ( 
+        <>
+            <div className="back-button-container">
+                <Button>Voltar</Button>
+            </div>
+            <div className="task-details-container">
+                
+            </div>
+        </>
+     );
+}
+ 
+export default TaskDetails;
