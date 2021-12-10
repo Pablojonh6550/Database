3a37e19556831afa8b7106e0e329c2b053d5977a
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-10 19:31:24 -0300

    3.5

--- react_js/src/App.jsx
+++ react_js/src/App.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {v4 as uuidv4} from 'uuid';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import './App.css';
+
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 import Button from './components/Button';
@@ -23,6 +25,17 @@ const App = () => {
       completed: true,
     }
   ]);
+
+  useEffect(()=>{
+    const fetchTasks = async() => {
+      const {data} = await axios.get("https://jsonplaceholder.cypress.io/todos?_limit=10");
+      
+      setTasks(data);
+    };
+
+    fetchTasks();
+  }, []);
+
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
         if (task.id == taskId) return {... task, completed: !task.completed};
