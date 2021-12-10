9b44fd342e31d773872336f4dea822428434128a
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-10 12:15:21 -0300

    2.9

--- react_js/src/components/Task.jsx
+++ react_js/src/components/Task.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {CgClose, CgCLose} from 'react-icons/cg'
+import {CgClose, CgInfo} from 'react-icons/cg'
 import './Task.css'
 
     const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
@@ -11,6 +11,9 @@ import './Task.css'
 
                 </div>
                 <div className='buttons-container'>
+                    <button className='see-task-details-button'>
+                        <CgInfo />
+                    </button>
                     <button className='remove-task-button' onClick={() => handleTaskDeletion(task.id)}>
                         <CgClose />
                     </button>
