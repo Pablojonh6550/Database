7ffe856dea965fa5adb4cf46d33a91de61871283
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-10 12:02:09 -0300

    2.7

--- react_js/src/components/Task.jsx
+++ react_js/src/components/Task.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Task.css'
-const Task = ({task, handleTaskClick}) => {
+
+    const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
 
         return (
             <div className='task-container' style={task.completed ? {borderLeft:'6px solid chartreuse'} : {}}>
@@ -8,12 +9,12 @@ const Task = ({task, handleTaskClick}) => {
                     {task.title}
 
                 </div>
+                <div className='buttons-container'>
+                    <button className='remove-task-button' onClick={() => handleTaskDeletion(task.id)}>X</button>
+                </div>
             </div>
         );
 
-    // return <div className="task-container">
-    //     {task.title}
-    // </div>;
-}
+    };
  
 export default Task;
--- react_js/src/components/tasks.jsx
+++ react_js/src/components/tasks.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Task from "./Task";
 
-const Tasks = ({tasks, handleTaskClick}) => {
+const Tasks = ({tasks, handleTaskClick, handleTaskDeletion}) => {
 
     return (
     <>
-        {tasks.map(task => <Task task={task} handleTaskClick={handleTaskClick} />)}
+        {tasks.map(task => (
+        <Task task={task} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
+        ))}
     </>
     );
 }
