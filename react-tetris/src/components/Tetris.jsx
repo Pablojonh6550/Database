44e471a8bdf57aa1b476fc12c23f790acb745060
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-14 15:35:21 -0300

    1.3

---
+++ react-tetris/src/components/Tetris.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+// Components ---> game manipulation.
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+
+const Tetris = () => {
+    return ( 
+        
+     );
+}
+ 
+export default Tetris;
