d1fca8b94c7fd7dcdbf467017bacf88e9ccdea5b
Pablojonh6550 <pablojonh6550@gmail.com>
2021-12-15 18:18:26 -0300

    2.2

--- react-tetris/src/gameHelpers.jsx
+++ react-tetris/src/gameHelpers.jsx
@@ -5,3 +5,10 @@ export const createStage = () =>
     Array.from(Array(STAGE_HEIGTH), () =>
         new Array(STAGE_WIDTH).fill([0, 'clear'])
     );
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+    for (let y = 0; y < array.length; y++) {
+        const element = array[y];
+        
+    }
+};
